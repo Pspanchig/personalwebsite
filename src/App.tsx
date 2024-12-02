e8c891a33c36ccfc603d7e2db80a3293a1637d92
Pablo Panchig <pspan@Santi>
2024-12-02 16:34:32 -0600

    PlaceHolder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import foto from './assets/mimor.jpg'
 
 function App() {
 
   return (
     <main>
-      <h1>This is my website</h1>
+      <h1>Mira mi amor lo logre!!!</h1>
+      <h3>Cree una web en la red profunda!!!</h3>
+      <p>Que el mundo vea nuestra foto :3</p>
+      <img src={foto} alt="mi amor y yo" />
     </main>
   )
 }
