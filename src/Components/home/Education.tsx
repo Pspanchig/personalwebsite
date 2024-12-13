65ed332ef51e4af63cb7b43f54357f71494467c0
Pablo Panchig <pspan@Santi>
2024-12-13 12:53:57 -0600

    404 page added, and Part of Education

---
+++ src/Components/home/Education.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Education.css'
+
+const Education: React.FC = () => {
+  return (
+    <article className='Education'>
+        <title>
+            
+        </title>
+    </article>
+  );
+}
+
+export default Education;
--- src/Components/home/UserPresentation.tsx
+++ src/Components/home/UserPresentation.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../Shared/css/General-Main.css'
+import './css/UserPresentation.css'
+import Education from './Education'
 
 const UserPresentation: React.FC = () => {
   return (
-    <section className='InformationContainer'>
-      
+    <section>
+      <div className='DivContainers'>
+        <div className='DivsDividerContainers' id='one'>
+          <div className='UpperDiv'>
+            <h1>Pablo S. Panchig</h1>
+            <h3>Software☻ Engineer</h3>
+          </div>
+          <div className='LowerDiv'>
+            <Education/>
+          </div>
+        </div>      
+        <div className='DivsDividerContainers' id='two'>
+          <div className='LowerDiv'>
+          <Education/>
+          </div>
+          <div className='UpperDiv'>Social Networks</div>
+        </div>      
+      </div>
+
     </section>
   )
 }
