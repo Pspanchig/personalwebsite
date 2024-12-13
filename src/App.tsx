49ba68fcc4752c86ac533e39b4f466f6e5f9828a
Pablo Panchig <pspan@Santi>
2024-12-13 12:57:05 -0600

    404 page fixed

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='/*' element={<NotFound/>}/>
+        <Route path='/NotFound' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
 
