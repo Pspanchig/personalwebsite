a1cab4b47855e9ec3370e2a28c5f39a79c67af23
Pablo Panchig <pspan@Santi>
2024-12-16 18:59:56 -0600

    translation of Education, name, education, navbar and mobile

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from '../Components/Shared/Navbar'
 import DisplayInfo from '../Components/NotFound/DisplayInfo'
 
 const NotFound: React.FC = () => {
   return (
     <main>
-        {/* <Navbar/> */}
         <DisplayInfo/>
     </main>
   )
