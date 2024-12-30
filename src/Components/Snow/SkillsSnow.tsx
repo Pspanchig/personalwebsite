da7414612104fb3feb772fa11e62cc21e9a97f25
Pablo Panchig <pspan@Santi>
2024-12-30 16:50:36 -0600

    Snow page finished

---
+++ src/Components/Snow/SkillsSnow.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './css/SkillsSnow.css';
+
+const SkillsSnow: React.FC = () => {
+  return (
+    <section className='SkillsSnow'>
+      <div className='SkillsContainerSnow'>
+        <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Fundamentals Of Programing</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Object-oriented Programming</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Introduction to Web Development</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/17483848/pexels-photo-17483848/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-depicts-a-look-inside-how-ai-microchips-are-designed-it-was-created-by-champ-panupong-techawongthawon-as-part-of-the-v.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Data Structures and Algorithms</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/6636458/pexels-photo-6636458.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Digital Circuits</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/5582591/pexels-photo-5582591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Computer Science</p>
+              <h1>Computer Organization and Architecture</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+          <div className='SkillItem'>
+            <img id='SkillImg' src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Skill Img" />
+            <div className='SkillItemDescription'>
+              <p>Software Engineering</p>
+              <h1>Back-end Web Development</h1>
+              <div className='SkillItemMore'>
+                <img src="https://www.svgrepo.com/show/512467/menu-navigation-grid-1530.svg" alt="atribute icon" />
+                <p>Read more</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+  );
+}
+
+export default SkillsSnow;
