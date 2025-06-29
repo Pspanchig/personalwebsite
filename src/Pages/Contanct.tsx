27c20ac7fbb82fd907c3be971c06a0a713518c97
pspanchig <pspanchig@gmail.com>
2025-06-29 13:02:59 -0500

    Auto scroll top fixed

--- src/Pages/Contanct.tsx
+++ src/Pages/Contanct.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Navbar from '../Components/Shared/Navbar';
 import './css/Contact.css'
 import MainContact from '../Components/Contact/MainContact';
 
-const Contact: React.FC = () => {
-        
-    const [language] = useState<string>('En')    
+const Contact: React.FC = () => {        
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const scrollToTop = () => {
+        spanRef.current?.scrollIntoView({ behavior: 'instant' });
+    };
+    scrollToTop();
+  }, []);
+  
 
   return (
     <main className='Contact'>
-        <Navbar language={language}/>
+        <span ref={spanRef}></span>            
+        <Navbar language={'En'}/>
         <MainContact/>
     </main>
   );
--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './css/Projects.css'
 import Navbar from '../Components/Shared/Navbar';
 import ProjectsMenu from '../Components/Projects/ProjectsMenu';
 
 const Projects: React.FC = () => {
+
+    const spanRef = useRef<HTMLSpanElement>(null);
+  
+    useEffect(() => {
+      const scrollToTop = () => {
+          spanRef.current?.scrollIntoView({ behavior: 'instant' });
+      };
+      scrollToTop();
+    }, []);
+    
   return (
     <main className='Projects'>
+      <span ref={spanRef}></span>
       <Navbar language='En'/>
       <ProjectsMenu/>      
     </main>
